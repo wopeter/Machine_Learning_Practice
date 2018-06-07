536c600ecdd33cf1074935ba4c99c1a4fef3cbdd
wopeter <pengtemail@163.com>
2018-06-07 23:34:08 +0800

    add code

--- svm.py
+++ svm.py
@@ -4,7 +4,8 @@ Created on Mon Jun  4 23:55:19 2018
 
 @author: pengte
 """
-
+from numpy import *
+import matplotlib.pyplot as plt
 def loadDataSet(fileName):
     dataMat = []
     labelMat = []
@@ -16,64 +17,112 @@ def loadDataSet(fileName):
     return dataMat, labelMat
 
 def selectJrand(i, m):
-	j = i
-	while(j == i):
-		j = int(random.uniform(0, m))
-	return j
-	
+    j = i
+    while(j == i):
+        j = int(random.uniform(0, m))
+    return j
+    
 def clipAlpha(aj, H, L):
-	if aj > H:
-		aj = H
-	if L > aj:
-		aj = L
-	return aj
+    if aj > H:
+        aj = H
+    if L > aj:
+        aj = L
+    return aj
 
 def smoSimple(dataMatIn, classLabels, C, toler, maxIter): 
-	dataMatrix = mat(dataMatIn)
-	labelMat = mat(classLabels).transpose()
-	m, n = shape(dataMatrix)
-	
-	b = 0
-	alphas = mat(zeros((m, 1)))
-	
-	iter = 0
-	while(iter < maxIter):
-		pass
-		
-	return b, alphas
+    dataMatrix = mat(dataMatIn)
+    labelMat = mat(classLabels).transpose()
+    m, n = shape(dataMatrix)
+    
+    b = 0
+    alphas = mat(zeros((m, 1)))
+    
+    iter = 0
+    while(iter < maxIter):
+        alphaPairsChanged = 0
+        for i in range(m):
+            fXi = float(multiply(alphas, labelMat).T * (dataMatrix * dataMatrix[i, :].T)) + b
+            Ei = fXi - float(labelMat[i])
+            
+            if ((labelMat[i] * Ei < -toler) and (alphas[i] < C)) or ((labelMat[i] * Ei > toler) and (alphas[i] > 0)):
+                j = selectJrand(i, m)
+                fXj = float(multiply(alphas, labelMat).T * (dataMatrix * dataMatrix[j, :].T)) + b
+                Ej = fXj - float(labelMat[j])
+                alphaIold = alphas[i].copy()
+                alphaJold = alphas[j].copy()
+                
+                if (labelMat[i] != labelMat[j]):
+                    L = max(0, alphas[j] - alphas[i])
+                    H = min(C, C + alphas[j] - alphas[i])
+                else:
+                    L = max(0, alphas[j] + alphas[i] - C)
+                    H = min(C, alphas[j] + alphas[i])
+                if L == H:
+                    print("L == H")
+                    continue
+                eta = 2.0 * dataMatrix[i, :] * dataMatrix[j, :].T - dataMatrix[i, :] * dataMatrix[i, :].T - dataMatrix[j, :] * dataMatrix[j, :].T
+                if eta >= 0:
+                    print("eta >= 0")
+                    continue
+                alphas[i] += labelMat[j] * labelMat[i] * (alphaJold - alphas[j])
+                b1 = b - Ei - labelMat[i] * (alphas[i] - alphaIold) * dataMatrix[i, :] * dataMatrix[i, :].T - labelMat[j] * (alphas[j] - alphaJold) * dataMatix[i, :] * dataMatrix[j, :].T
+                b2 = b - Ej - labelMat[i] * (alphas[i] - alphaIold) * dataMatrix[i, :] * dataMatrix[j, :].T - labelMat[j] * (alphas[j] - alphsJold) * dataMatix[j, :] * dataMatrix[j, :].T
+                if (0 < alphas[i] ) and (C > alphas[i]):
+                    b = b1
+                elif (o < alphas[j]) and (C < alphas[j]):
+                    b = b2
+                else:
+                    b = (b1 + b2) / 2.0
+                alphaPairsChanged += 1
+                print("iter: %d i:%d, pairs changed %d" % (iter, i, alphaPairsChanged))
+        if(alphaPairsChanged == 0):
+            iter += 1
+        else:
+            iter = 0
+        print("iteration number: %d" % iter)
+        
+    return b, alphas
 
 def calcWs(alphas, dataArr, classLabels):
-	X = mat(dataArr)
-	labelMat = mat(classLabels).transpose()
-	m, n = shape(X)
-	w = zeros((n, 1))
-	for i in range(m):
-		w += multiply(alphas[i] * labelMat[i], X[i, :].T)
-	return w
-	
+    X = mat(dataArr)
+    labelMat = mat(classLabels).transpose()
+    m, n = shape(X)
+    w = zeros((n, 1))
+    for i in range(m):
+        w += multiply(alphas[i] * labelMat[i], X[i, :].T)
+    return w
+    
 def plotfig_SVM(xMat, yMat, ws, b, alphas):
-	xMat = mat(xMat)
-	yMat = mat(yMat)
-	
-	b = array(b)[0]
-	fig = plt.figure()
-	ax = fig.add_subplot(111)
-	
-	ax.scatter(xMat[:, 0].flatten().A[0], xMat[:, 1].flatten().A[0])
-	x = arange(-1.0, 10.0, 0.1)
-	y = (-b - ws[0, 0]*x)/ws[1, 0]
-	ax.plot(x, y)
-	for i in range(shape(yMat[0, :])[1]):
-		if yMat[0, i] > 0:
-			ax.plot(xMat[i, 0], xMat[i, 1], 'cx')
-		else:
-			ax.plot(xMat[i, 0], xMat[i, 1], 'kp')
-	for i in range(100):
-		if alphas[i] > 0.0
-			ax.plot(xMat[i, 0], xMat[i, 1], 'ro')
-	plt.show()
-	
+    xMat = mat(xMat)
+    yMat = mat(yMat)
+    
+    b = array(b)[0]
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    
+    ax.scatter(xMat[:, 0].flatten().A[0], xMat[:, 1].flatten().A[0])
+    x = arange(-1.0, 10.0, 0.1)
+    y = (-b - ws[0, 0]*x)/ws[1, 0]
+    ax.plot(x, y)
+    for i in range(shape(yMat[0, :])[1]):
+        if yMat[0, i] > 0:
+            ax.plot(xMat[i, 0], xMat[i, 1], 'cx')
+        else:
+            ax.plot(xMat[i, 0], xMat[i, 1], 'kp')
+    for i in range(100):
+        if alphas[i] > 0.0:
+            ax.plot(xMat[i, 0], xMat[i, 1], 'ro')
+    plt.show()
+    
 if __name__ == "__main__":
-	pass
-	
+    print('/n' * 3)
+    print("b = %f" % b)
+    print("alphas[alphas>0] = ", alphas[alphas > 0])
+    print("shape(alphas[alphas > 0]) = ", shape(alphas[alphas > 0]))
+    for i in range(100):
+        if alphas[i] > 0:
+            prin(dataArr[i], labelArr[i])
+    ws = calWs(alphas, dataArr, labelArr)
+    plotfig_SVM(dataArr, labelArr, ws, b, alphas)
+
     
